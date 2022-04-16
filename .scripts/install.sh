a1a07d58c5e1bb9754e8e1c5617b135ee0e6826c
GRIPPERTV <GRIPPERTV0@gmail.com>
2022-04-16 05:12:38 -0300

    Add files via upload

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -2,21 +2,37 @@
 mv pacman.conf /etc/
 pacman -Syu
 
-# Install drivers, compilers, libs and steam
-pacman -S elogind base-devel mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader rust go python paru xorg xorg-init gamemode lib32-gamemode steam
+# Install libs/drivers/compilers and steam
+pacman -S base-devel elogind mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader rust go python paru xorg xorg-init gamemode lib32-gamemode fontconfig steam
 
-# Theme and fonts
+# Install theme/fonts and cache fonts
 paru yaru
 paru helvetica
 paru nerd-font
 paru jetbrains
+fc-cache -fv
 
-# Setup settings
-mv ../*.* $HOME
+# Download st, dwm and dwmblocks
+# Don't install yet or it will use default config.def.h
+mkdir $HOME/.pkg
+cd $HOME/.pkg
+git clone https://git.suckless.org/st
+git clone https://git.suckless.org/dwm
+git clone https://github.com/torrinfail/dwmblocks
+
+# Setup dotfiles
+cd $HOME/dotfiles
+mv *.* $HOME
 rm $HOME/.scripts/install.sh $HOME/.scripts/pacman.conf
 
-# dwm
+# Now we can install st, dwm and dwmblocks
+cd $HOME/.pkg
+cd st && make install
+cd ../dwm && make install
+cd ../dwmblocks && make install
 
-
-# Misc
+# Install misc
 pacman -S htop neofetch
+
+# Done!
+rm dotfiles
