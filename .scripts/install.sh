63b433e68a61ad41e01e4929ef77ed1c858ed360
GRIPPERTV <GRIPPERTV0@gmail.com>
2022-05-01 23:52:12 +0000

    Update install.sh

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,14 +1,28 @@
+# Set GRUB_CMDLINE_LINUX_DEFAULT and update grub config
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=0 quiet mitigations=off zswap.enabled=0 nowatchdog"/' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# Configure sysctl.conf
+sudo mv sysctl.conf /etc/
+
+# Configure modules blacklist
+sudo mv blacklist.conf /etc/modprobe.d/
+
+# Configure HDD rules
+sudo echo 'ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"' > /etc/udev/rules.d/60-hdd.rules
+
 # Install paru, configure pacman and update mirrors
 cd $HOME && mkdir .pkg && git clone https://aur.archlinux.org/paru.git
-cd paru && makepkg -si && cd .. && sudo rm paru -r
+cd paru && makepkg -si && cd .. && sudo rm -r paru
 sudo mv pacman.conf /etc/ && paru -Syu
 
 # Install base, compilers, drivers and libs
-sudo pacman -S --needed base-devel go rust python mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader gamemode lib32-gamemode lib32-fontconfig fontconfig pulseaudio xorg xorg-xinit
+sudo pacman -S --needed base-devel go rust python mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader intel-media-driver intel-media-sdk gamemode lib32-gamemode lib32-fontconfig fontconfig pulseaudio xorg xorg-xinit
 
 # Install theme, fonts and cache all fonts
 paru yaru-gtk-theme
 paru ttf-jetbrains-mono
+paru ttf-ms-fonts # For Windows apps
 paru ttf-twemoji
 fc-cache -fv
 
@@ -17,6 +31,7 @@ cd $HOME/.pkg
 git clone https://git.suckless.org/st
 git clone https://git.suckless.org/dwm
 git clone https://github.com/torrinfail/dwmblocks
+git clone https://github.com/djpohly/dwl
 
 # Install st patches (scrollback-mouse, clipboard and dynamic cursor color)
 cd st
@@ -45,16 +60,16 @@ cd ../dwmblocks && sudo make install
 cd ../dwl && sudo make install
 
 # Install needed dotfiles packages
-sudo pacman -S --needed feh zsh scrot xclip && paru rlaunch
+echo y | sudo pacman -S --needed feh zsh scrot xclip && paru rlaunch
 
 # Install Oh My Zsh (default theme)
 cd $HOME && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install steam, htop, neofetch and ungoogled
-sudo pacman -S --needed steam htop neofetch && paru ungoogled
+echo y | sudo pacman -S --needed steam htop neofetch && paru ungoogled
 
-# Clear pacman/paru
-sudo pacman -R $(pacman -Qtdq) && paru -Scc
+# Remove cached/unneeded packages and diffs
+echo y | sudo pacman -R $(pacman -Qtdq) && paru -Scc
 
-# Done!
-echo "Done!"
+# Reboot
+sudo reboot
